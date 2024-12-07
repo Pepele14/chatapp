5eadc6ed04257c0ca88c86a5cad7fc9eae29e67b
Giuseppe Moschetta <giu.mos.dev@gmail.com>
2024-12-07 15:36:43 +0100

    authentication middleware create

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 
 import { connectDB } from "./lib/db.js";
 import authRoutes from "./routes/auth.route.js";
@@ -10,6 +11,7 @@ const app = express();
 const PORT = process.env.PORT
 
 app.use(express.json()) //middleware to extract json data out of body of the submitted form
+app.use(cookieParser()) // parsing the cookie
 
 app.use("/api/auth", authRoutes) //route for authentication
 
