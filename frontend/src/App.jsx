5cdb7a88c8409ff9e7f445fa199724dcd224f6aa
Giuseppe Moschetta <giu.mos.dev@gmail.com>
2024-12-14 20:08:23 +0100

    import axios & export instance create

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,7 @@ import SignUpPage from "./pages/SignUpPage";
 import LoginPage from "./pages/LoginPage";
 import SettingsPage from "./pages/SettingsPage";
 import ProfilePage from "./pages/ProfilePage";
+// import { axiosInstance } from "./lib/axios";
 
 
 
