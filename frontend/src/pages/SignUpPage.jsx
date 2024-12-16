27919b4869d8a2252c8773f69025f9e7fcf2c16a
Giuseppe Moschetta <giu.mos.dev@gmail.com>
2024-12-16 18:04:13 +0100

    fixes - password label

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useAuthStore } from "../store/useAuthStore";
-import { Mail, MessageSquare, User } from "lucide-react";
+import { Eye, EyeOff, Lock, Mail, MessageSquare, User } from "lucide-react";
 
 const SignUpPage = () => {
   const [showPassword, setShowPassword] = useState(false);
@@ -62,18 +62,46 @@ const SignUpPage = () => {
               type="email"
               className={`input input-bordered w-full pl-10`}
               placeholder="you@example.com"
-              value={formData.fullName}
-              onChange={(e) => setFormData({ ...formData, fullName: e.target.value})}
+              value={formData.email}
+              onChange={(e) => setFormData({ ...formData, email: e.target.value})}
               />
+            </div>
+           </div>
+
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text font-medium">Password</span>
+          </label>
+          <div className="relative">
+            <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+              <Lock className="size-5 text-base-content/40" />
+            </div>
+            <input
+              type={showPassword ? "text" : "password"}
+              className={`input input-bordered w-full pl-10`}
+              placeholder="•••••••••"
+              value={formData.password}
+              onChange={(e) => setFormData({ ...formData, password: e.target.value})}
+              />
+
+              <button
+               type="button"
+               className="absolute inset y-0 right-0 pr-3 flex items-center"
+               onClick={() => setShowPassword(!showPassword)}
+               >
+                {
+                  showPassword ? (
+                    <EyeOff className="size-5 text-base-content/40" />
+                  ) : (
+                    <Eye className="size-5 text-base-content/40" />
+                  )
+                }
+               </button>
           </div>
          </div>
-
       </form>
-
       </div>
-
      </div>
-
     </div>
   )
 }
