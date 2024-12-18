37277a129e1a208bf2930b86d1d9296752324fca
Giuseppe Moschetta <giu.mos.dev@gmail.com>
2024-12-18 17:53:26 +0100

    new pattern component create

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { useAuthStore } from "../store/useAuthStore";
 import { Eye, EyeOff, Loader2, Lock, Mail, MessageSquare, User } from "lucide-react";
 import { Link } from "react-router-dom";
+import AuthImagePattern from "../components/AuthImagePattern";
 
 const SignUpPage = () => {
   const [showPassword, setShowPassword] = useState(false);
@@ -125,6 +126,14 @@ const SignUpPage = () => {
         </div>
        </div>
      </div>
+
+     {/* { right side } */}
+
+     <AuthImagePattern
+        title="Join our community"
+        subtitle="Connect with friends, share moments, and stay in touch with your loved ones."
+     />
+
     </div>
   )
 }
