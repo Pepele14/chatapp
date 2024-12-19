97df739a4cdad56e46f74001fe34a43f2a03b267
Giuseppe Moschetta <giu.mos.dev@gmail.com>
2024-12-19 18:13:57 +0100

    fix typo cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 import { connectDB } from "./lib/db.js";
 import authRoutes from "./routes/auth.route.js";
@@ -13,7 +14,14 @@ const PORT = process.env.PORT
 
 app.use(express.json()) //middleware to extract json data out of body of the submitted form
 app.use(cookieParser()) // parsing the cookie
+app.use(cors({
+    origin: "http://localhost:5173",
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // List allowed HTTP methods
+    credentials: true,
 
+
+}
+))
 app.use("/api/auth", authRoutes) //route for authentication
 app.use("/api/message", messageRoutes)
 
