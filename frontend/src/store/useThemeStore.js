2d15185938fdde252379ff235b25b11ea2ce2794
Giuseppe Moschetta <giu.mos.dev@gmail.com>
2024-12-24 19:16:05 +0100

    themes save to local storage

---
+++ frontend/src/store/useThemeStore.js
@@ -0,0 +1,10 @@
+//saving themes to local storage 
+import { create } from "zustand";
+
+export const useThemeStore = create((set) => ({
+    theme: localStorage.getItem("chat-theme") || "coffee",
+    setTheme: (theme) => {
+        localStorage.setItem("chat-theme", theme);
+        set({ theme })
+    }
+}))
